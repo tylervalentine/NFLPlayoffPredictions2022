76aef319ffe3a1823be66a1ebb5cf32170257e06
Tyler Valentine <valentinet02@moravian.edu>
2022-01-12 18:15:23 -0500

    Specify host

--- .gitignore/info.php
+++ .gitignore/info.php
@@ -1,6 +1,6 @@
 
 <?php
-$link = mysqli_connect("127.0.0.1", "nfl", "predictions", "NFLPlayoffPredictions2022");
+$link = mysqli_connect("192.168.1.7", "nfl", "predictions", "NFLPlayoffPredictions2022");
 
 if($link === false)
 {
